bfafe715e5b764ab39c3584c87339eb85377fc37
Yamaguchi Mayo <184481668+Mayo-26329@users.noreply.github.com>
2025-09-02 14:38:16 +0900

    PrefDataStoreの実装

--- app/src/main/java/jp/ac/meijou/android/s241205177/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205177/MainActivity.java
@@ -18,6 +18,7 @@ import jp.ac.meijou.android.s241205177.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+   private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        prefDataStore = prefDataStore.getInstance(this);
+        binding.savebutton.setOnClickListener(view ->{
+            var text =binding.editTextText.getText().toString();
+            prefDataStore.setString("name",text);
         });
 
         binding.text.setText(R.string.text1);
